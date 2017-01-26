5c98fe8ac6f5c4ea095d28335a3b05543a7adf31
Troels Thomsen <troels@thomsen.io>
2017-01-26 21:14:42 +0100

    Implement channel reader

--- io.go
+++ io.go
@@ -2,6 +2,7 @@ package shuttle
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 )
 
@@ -13,4 +14,32 @@ func Copy(ch chan<- []byte, r io.Reader) error {
 	}
 
 	return scanner.Err()
+}
+
+type reader struct {
+	buf *bytes.Buffer
+	ch  <-chan []byte
+}
+
+func Reader(ch <-chan []byte) io.Reader {
+	return &reader{
+		new(bytes.Buffer),
+		ch,
+	}
+}
+
+func (r *reader) Read(p []byte) (int, error) {
+	if r.buf.Len() == 0 {
+		p, ok := <-r.ch
+		if !ok {
+			return 0, io.EOF
+		}
+
+		_, err := r.buf.Write(p)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return r.buf.Read(p)
 }
--- io_test.go
+++ io_test.go
@@ -2,6 +2,7 @@ package shuttle
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,4 +20,21 @@ func TestCopy(t *testing.T) {
 
 	assert.Equal(t, "foo\n", string(<-ch))
 	assert.Equal(t, "bar baz\n", string(<-ch))
+}
+
+func TestRead(t *testing.T) {
+	ch := make(chan []byte, 2)
+	ch <- []byte("hello")
+	ch <- []byte("world")
+
+	r := Reader(ch)
+
+	p := make([]byte, 4)
+	n, _ := r.Read(p)
+	assert.Equal(t, "hell", string(p[:n]))
+	n, _ = r.Read(p)
+	assert.Equal(t, "o", string(p[:n]))
+	p = make([]byte, 6)
+	n, _ = r.Read(p)
+	assert.Equal(t, "world", string(p[:n]))
 }
